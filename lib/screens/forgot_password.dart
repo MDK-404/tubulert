79d4248f8970016d01543f90f2e9dc149c955864
Muhammad Daniyal Khan <mdk968196@gmail.com>
2024-07-25 11:58:22 -0600

    forgot screen

--- lib/screens/forgot_password.dart
+++ lib/screens/forgot_password.dart
@@ -14,25 +14,29 @@ class _ForgotPasswordState extends State<ForgotPassword> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(Texts.appName),
-              const SizedBox(height: 50),
-              Text(
-                'Forgot Password',
-                style: Styles.signupStyle,
-              ),
-              Text(
-                'Entr your email to recieve code',
-                style: GoogleFonts.openSans(
-                    textStyle: const TextStyle(
-                        color: Color(0xFFB70063), fontSize: 12)),
-              ),
-            ],
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(Texts.appName, style: Styles.appNameStyle),
+                const SizedBox(height: 50),
+                Text(
+                  'Forgot Password',
+                  style: Styles.signupStyle,
+                ),
+                Text(
+                  'Entr your email to recieve code',
+                  style: GoogleFonts.openSans(
+                      textStyle: const TextStyle(
+                          color: Color(0xFFB70063), fontSize: 12)),
+                ),
+              ],
+            ),
           ),
         ),
       ),
